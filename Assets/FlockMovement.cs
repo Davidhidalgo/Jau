7e2ace361359a698a302a0f2a8c99181f8e04d14
David Hidalgo <dhidalgo@agroptima.com>
2020-04-01 14:05:35 +0200

    Initial commit

---
+++ Assets/FlockMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockMovement : MonoBehaviour
+{
+  public Rigidbody rb;
+  public GameObject Player;
+  public float Force = 150;
+  private float DistanceTrigger = 5;
+  void Update()
+  {
+    float Distance = Vector3.Distance(Player.transform.position, transform.position);
+    Vector3 Direction = Player.transform.position - transform.position;
+    Vector3 DirectionApplied = -Direction.normalized * (Force * Time.deltaTime);
+    Debug.Log(Distance);
+    Debug.Log(DistanceTrigger);
+    if (Distance < DistanceTrigger)
+    {
+      rb.AddForce(DirectionApplied);
+    }
+    Debug.DrawLine(transform.position, transform.position + DirectionApplied, Color.red);
+  }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public Rigidbody rb;
+  public float Force = 800f;
+
+  void Update()
+  {
+    if (Input.GetKey("w"))
+    {
+      rb.AddForce(0, 0, Force * Time.deltaTime);
+    }
+    if (Input.GetKey("s"))
+    {
+      rb.AddForce(0, 0, -Force * Time.deltaTime);
+    }
+    if (Input.GetKey("d"))
+    {
+      rb.AddForce(Force * Time.deltaTime, 0, 0);
+    }
+    if (Input.GetKey("a"))
+    {
+      rb.AddForce(-Force * Time.deltaTime, 0, 0);
+    }
+  }
+}
