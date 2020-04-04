d7bd93b4e1f604093c2a2dddfb0cb26f03659d32
David Hidalgo <dhidalgo@agroptima.com>
2020-04-04 16:41:57 +0200

    Unified FlockController and FlockMovement

--- Assets/FlockController.cs
+++ Assets/FlockController.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class FlockController : MonoBehaviour
 {
+  public float Force = 150;
+  public float DistanceTrigger = 10;
   private GameObject Player;
+  private float Distance;
+  private Vector3 moveDirection;
+  void Update()
+  {
+    CalcRepelPlayer();
+  }
+
+  void FixedUpdate()
+  {
+    RepelPlayer();
+  }
+
   public void SetPlayer(GameObject player)
   {
     Player = player;
-    gameObject.GetComponent<FlockMovement>().SetPlayer(player);
-  }
-  void Start()
-  {
-
   }
 
-  // Update is called once per frame
-  void Update()
+  void CalcRepelPlayer()
   {
+    Distance = Vector3.Distance(Player.transform.position, transform.position);
+    Vector3 Direction = Player.transform.position - transform.position;
+    moveDirection = -Direction.normalized * (Force * Time.deltaTime);
+    moveDirection.y = 0;
+    Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+    transform.rotation = newRotation;
+    Gizmos.color = Color.blue;
+  }
 
+  void RepelPlayer()
+  {
+    if (Distance < DistanceTrigger)
+    {
+      Rigidbody rb = GetComponent<Rigidbody>();
+      rb.AddForce(moveDirection * (DistanceTrigger / Distance));
+    }
+    Debug.DrawLine(transform.position, transform.position + moveDirection, Color.red);
   }
 }
